146160520a4cf8bc75e0b7d5fd177fcff0fd8090
roman-kiselenko <roman.kiselenko.dev@gmail.com>
2025-02-21 19:28:21 +0200

    Update

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "rkiselenko.dev",
 	url: "https://rkiselenko.dev/",
 	language: "en",
-	description: "Insights from the Daily Life of a Software Developer",
+	description: "Insights from the Daily Life of a Engineer",
 	author: {
 		name: "Roman Kiselenko",
 		email: "roman.kiselenko.dev@gmail.com",
